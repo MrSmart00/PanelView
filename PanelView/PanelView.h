f28ae6f58b29ab7e71f2a7a48b6dd82d01c9bdf7
Hiroya Hinomori <nexearth@gmail.com>
2019-04-13 01:12:06 +0900

    Initial Commit

---
+++ PanelView/PanelView.h
@@ -0,0 +1,19 @@
+//
+//  PanelView.h
+//  PanelView
+//
+//  Created by 日野森寛也 on 2019/04/13.
+//  Copyright © 2019 Hiroya Hinomori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PanelView.
+FOUNDATION_EXPORT double PanelViewVersionNumber;
+
+//! Project version string for PanelView.
+FOUNDATION_EXPORT const unsigned char PanelViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PanelView/PublicHeader.h>
+
+
